76902cbef60dfb194fa391381a5b11d9673d0c01
Varun Belani <varunbelani7@gmail.com>
2019-09-23 19:46:00 -0400

    Add files via upload

---
+++ Java/Pseudo-Machine Learning/README.md
@@ -0,0 +1,1 @@
+Please run the UberLearning.java to execute program.
---
+++ Java/Pseudo-Machine Learning/UberLearning.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class UberLearning
+{
+   public static void main(String args[])
+   {
+      Scanner input = new Scanner(System.in);
+      Learner bot = new Learner();
+      Random mysteryMachine = new Random();
+      bot.createRoute();
+        
+      
+   }
+}
