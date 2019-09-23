90bef1c3c91faeac7d3c7b660b15a745a482b846
Varun Belani <varunbelani7@gmail.com>
2019-09-23 19:52:56 -0400

    Create TravellingSalesmen.java

---
+++ Java/Path-finder Algo/TravellingSalesmen.java
@@ -0,0 +1,13 @@
+// runs salesmen user interface
+public class TravellingSalesmen
+{
+   public static void main(String args[])
+   {
+      Salesmen activateAlgo = new Salesmen();
+      
+      while(true)
+      {
+         activateAlgo.printMenu();
+      }
+   }
+}
