4168e976db06a8d9bfaab8c40b7ce5978ff37bb1
Varun Belani <varunbelani7@gmail.com>
2021-05-17 10:52:18 -0400

    Delete Python directory

--- Python/GPA.py
+++
@@ -1,5 +0,0 @@
-numCredits = int(input("Enter number of credits: "))
-majorGPA = float(input("Enter GPA for the major: "))
-overallGPA = float(input("Enter overall GPA: "))
-
-if numCreddits >= 120 and majorGPA >= 2.0 and overall
